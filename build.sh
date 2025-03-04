d5dfa0615241af304a976d65c2c4ac91482081f5
Romain Roffé <romain@kyber.media>
2025-03-04 15:06:13 +0100

    build/linux: Switch to ffmpeg master
    SHA1: f76195ff656d6bea68feee783160652e2b3e3d60

--- build.sh
+++ build.sh
@@ -193,8 +193,9 @@ build_decoder() {
     # FFmpeg
     echo "Build FFmpeg"
 
-    git clone --depth 1 --branch lcevc_generic.0 https://github.com/rofferom/FFmpeg.git "$LINUX_WORKDIR/ffmpeg"
+    git clone https://github.com/ffmpeg/FFmpeg.git "$LINUX_WORKDIR/ffmpeg"
     cd "$LINUX_WORKDIR/ffmpeg"
+    git checkout f76195ff656d6bea68feee783160652e2b3e3d60
 
     ./configure --prefix=$LINUX_ROOTFS \
         --enable-shared \
